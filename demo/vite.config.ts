4e330a4d332be3428f8a4ac3dfab17e53c3de5b2
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-04-26 12:49:42 -0400

    chore: setup vite-ssr demo

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,8 +1,10 @@
 import compress from 'vite-plugin-compress'
+import viteSSR from 'vite-ssr/plugin'
 import type { UserConfig } from 'vite'
 
 const config: UserConfig = {
   plugins: [
+    viteSSR(),
     compress({
       verbose: true,
     }),
