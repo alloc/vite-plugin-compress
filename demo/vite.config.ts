28e95dfaf9f2061db9d3214b80e214bb40dfb8a1
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-04-26 12:50:05 -0400

    chore: remove experimental plugins from demo

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,12 +1,8 @@
-import rehost from 'vite-plugin-rehost'
 import compress from 'vite-plugin-compress'
-import { publicHash } from 'vite-plugin-public'
 import type { UserConfig } from 'vite'
 
 const config: UserConfig = {
   plugins: [
-    rehost(),
-    publicHash(),
     compress({
       verbose: true,
     }),
